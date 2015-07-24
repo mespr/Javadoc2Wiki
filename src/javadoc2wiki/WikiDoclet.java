4d0ed1709528c3a234c4af920eaf67bdfd182dce
Michael Sprague <michael@sprague.com>
2015-07-24 09:53:34 -0400

    fixed method name style

--- src/javadoc2wiki/WikiDoclet.java
+++ src/javadoc2wiki/WikiDoclet.java
@@ -5,57 +5,88 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.sun.javadoc.ClassDoc;
+import com.sun.javadoc.Doc;
 import com.sun.javadoc.FieldDoc;
 import com.sun.javadoc.MethodDoc;
+import com.sun.javadoc.ParamTag;
 import com.sun.javadoc.Parameter;
 import com.sun.javadoc.RootDoc;
 import com.sun.javadoc.Tag;
 import com.sun.javadoc.Type;
 
 public class WikiDoclet {
+	static String packageName;
     public static boolean start(RootDoc root) {
+
         for (ClassDoc cls : root.classes()) {
+        	if (!cls.isOrdinaryClass()) continue;
         	BufferedWriter out = null;
         	try  
         	{
-        	    FileWriter fstream = new FileWriter("Class"+cls.name()+".txt"); //true tells to append data.
+        	    FileWriter fstream = new FileWriter(makeWikiName(cls.name())+".txt");
         	    out = new BufferedWriter(fstream);
         	    
         	    // Wiki header. TOPICPARENT is set to package name and assumed to be created manually.
         	    // We could generate it from info found in package-info.java, but I would prefer
         	    // the flexibility of twiki markup for providing an overview
-        	    String packageName = cls.containingPackage().name();
+        	    packageName = makeWikiName(cls.containingPackage().name());
         	    out.write("%META:TOPICPARENT{name=\""+packageName+"\"}%\n");
-        	    out.write("---+"+cls.name()+"\n\n");
+        	    out.write("%INCLUDE{WebPageHeader}%\n");
+        	    TocWriter.add(cls,cls.name(),0,makeWikiName(cls.name()));
         	    out.write(cls.commentText()+"\n");
        	    
+        	    TocWriter.add(cls,"Summary of Fields",1);
         	    out.write("\n---++ Summary of Fields\n");
         	    out.write("|*Modifier and Type*|*Field*|\n");
         	    for (FieldDoc field : cls.fields()) {
-        	    	out.write("|"+field.modifiers()+" "+buildTypeName(field.type())+"|"+field.name());
+        	    	out.write("| ="+field.modifiers()+" "+buildTypeName(field.type())+"= |"+field.name()+" ");
         	    	if (!field.commentText().isEmpty()) {
-        	    		out.write("<br>"+getFirstLine(field.commentText()));
+        	    		out.write("<br>"+getFirstLine(field));
         	    	}
         	    	out.write("|\n");
         	    }
 
+        	    TocWriter.add(cls,"Summary of Methods",1);
         	    out.write("\n---++ Summary Methods\n");
         	    out.write("|*Modifier and Type*|*Method*|\n");
         	    for (MethodDoc method : cls.methods()) {
-        	    	out.write("|"+method.modifiers()+" "+buildTypeName(method.returnType())+"|"+buildMethodStatement(method));
+        	    	out.write("| ="+method.modifiers()+" "+buildTypeName(method.returnType())+"= | ="+buildMethodStatement(method)+"= ");
         	    	if (!method.commentText().isEmpty()) {
-        	    		out.write("<br>"+getFirstLine(method.commentText()));
+        	    		out.write("<br>"+getFirstLine(method));
         	    	}
         	    	out.write("|\n");
         	    }
 
+        	    TocWriter.add(cls,"Method Details",1);
         	    out.write("\n---++ Method Details\n");
         	    for (MethodDoc method : cls.methods()) {
-        	    	out.write("\n---+++ "+method.name()+"\n");
+            	    TocWriter.add(cls,method.name(),2);
+        	    	out.write("\n\n---+++ "+method.name()+"\n");
         	    	out.write("=="+method.modifiers()+" "+buildTypeName(method.returnType())+" "+buildMethodStatement(method)+"==\n\n");
+        	    	out.write(method.commentText()+"\n\n");
+        	    	if (method.paramTags().length > 0) {
+	        	    	out.write("|*Parameter*|*Description*|\n");
+	        	    	for (ParamTag param : method.paramTags()) {
+	        	    		out.write("|"+param.parameterName()+"|"+param.parameterComment()+"|\n");
+	        	    	}
+	        	    	for (Tag tag : method.tags("return")) {
+	        	    		String title = tag.name();
+	        	    		if (title.equals("@return")) {
+		        	    		out.write("\n---++++ Return Value");
+		        	    		out.write("\n"+tag.text());
+	        	    		} else {
+	        	    			out.write("\n---++++ "+tag.kind());
+	        	    			out.write("\n"+tag.text());
+	        	    		}
+	        	    	}
+        	    	}
         	    }
+        	    out.write("%META:FORM{name=\"WebForm\"}%\n");
+        	    out.write("%META:FIELD{name=\"Title\" attributes=\"M\" title=\"Title\" value=\""+cls.name()+"\"}%\n");
+        	    out.write("%META:FIELD{name=\"Description\" attributes=\"\" title=\"Description\" value=\""+getFirstLine(cls)+"\"}%\n");
 
         	    out.close();
           	}
@@ -65,6 +96,7 @@ public class WikiDoclet {
         	}
 
         }
+        TocWriter.close();
 
         return true;
     }	
@@ -84,16 +116,75 @@ public class WikiDoclet {
     private static String buildMethodStatement(MethodDoc method) {
     	String s = "";
     	s += method.name()+"(";
+    	String params = "";
     	for (Parameter param : method.parameters()) {
-    		s += buildTypeName(param.type())+" "+param.name()+",";
+    		params += buildTypeName(param.type())+" "+param.name()+",";
     	}
-    	s += (s.length()>0?s.substring(0,s.length()-1):"")+")";
+    	s += (params.length()>0?params.substring(0,params.length()-1):"")+")";
     	
     	return s;
     }
     
-    private static String getFirstLine(String s) {
-    	String lines[] = s.split(System.lineSeparator());
-    	return lines[0];
+    private static String getFirstLine(Doc doc) {
+    	String s = "";
+    	Tag[] tags = doc.firstSentenceTags();
+    	for (Tag tag : tags) {
+    		s += tag.text();
+    	}
+    	return s.replace("\n","");
+    }
+    
+    private static String makeWikiName(String s) {
+    	String result = "";
+    	String[] words = s.split("\\.");
+    	for (String word : words) {
+    		result += word.substring(0, 1).toUpperCase() + word.substring(1);
+    	}
+    	return result;
+    }
+    
+    private static class TocWriter {
+    	static HashMap<String, BufferedWriter> files = new HashMap<String, BufferedWriter>();
+
+		static BufferedWriter getFile(ClassDoc cls) {
+			BufferedWriter file = files.get(packageName);
+	    	try {
+	    		if (file == null) {
+		    	    FileWriter tocstream = new FileWriter(packageName+"TOC.txt");
+		    	    file = new BufferedWriter(tocstream);
+		    	    files.put(packageName,file);
+		    	    file.write("   * [["+packageName+"]["+cls.containingPackage().name()+"]]\n");
+	    		}
+	    	} catch (IOException e) {
+	    		System.err.println("Error: " + e.getMessage());
+	       	}
+	    	return file;
+    	}
+		static void add(ClassDoc cls, String title, int level) {
+			add(cls,title,level,cls.name()+"#" + title.replace(" ", "_"));
+		}
+		static void add(ClassDoc cls, String title, int level, String link) {
+			String line = "   "; //accommodate root package name
+			for (int i=0;i<=level;i++) { // starting with level zero prefix three spaces
+				line += "   ";
+			}
+			line += "* [["+link+"]["+title+"]]\n";
+			BufferedWriter out = getFile(cls);
+			try {
+				out.write(line);
+				out.flush();
+	    	} catch (IOException e) {
+	    		System.err.println("Error: " + e.getMessage());
+	       	}
+		}
+		static void close() {
+			try {
+				for (BufferedWriter file : files.values()) {
+				    file.close();
+				}
+	    	} catch (IOException e) {
+	    		System.err.println("Error: " + e.getMessage());
+	       	}
+		}
     }
 }
